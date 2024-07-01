91e858552b346825fb0da765489faeed764d55c7
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-07-01 09:37:58 -0500

    fix: :sparkles: Corregir nombre de endpoint en payloads.js
    Se ha corregido el nombre del endpoint en el archivo payloads.js de nuestra aplicación de información sobre lo relacionado con el espacio, como cohetes, naves, satélites, etc.

--- js/module/payloads.js
+++ js/module/payloads.js
@@ -1,12 +1,12 @@
 export const getAllPayloads = async () => {
 
-    let res = await fetch("https://api.spacexdata.com/v4/ships");
+    let res = await fetch("https://api.spacexdata.com/v4/payloads");
     let data = await res.json();
     return data;
 }
 export const getPayloadById = async (id) => {
 
-    let res = await fetch(`https://api.spacexdata.com/v4/ships/${id}`);
+    let res = await fetch(`https://api.spacexdata.com/v4/payloads/${id}`);
     let data = await res.json();
     return data;
 }
