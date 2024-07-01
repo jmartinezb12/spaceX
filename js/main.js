9f0af5d8c4d5d3c7f17f99f46a98c67335937f0f
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-07-01 12:27:22 -0500

    feat: :sparkles: Crear estructura inicial de index.html, style.css y main.js
    Se ha creado e implementado la estructura principal del archivo index.html, junto con los archivos style.css y main.js, para la primera visualización de la página.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-import { getAllRockets } from "./module/rockets.js";
+import { getAllRockets, getRocketById } from "./module/rockets.js";
 
-console.table(await getAllRockets());
+//console.table(await getAllRockets());
+console.table(await getRocketById("5e9d0d95eda69955f709d1eb"));
+
+document.addEventListener("DOMContentLoaded",  async()=>{
+    const params = new URLSearchParams(location.search);
+    
+});
