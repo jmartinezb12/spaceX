ef94294f8817e49f6e30d88662b2a0bcd6dd4665
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-07-01 04:17:56 -0500

    feat: :sparkles: Implementar métodos GET y getById en rockets.js
    Se han creado los métodos get y getById en el archivo rockets.js para realizar solicitudes GET y obtener la información de los cohetes en formato JSON.
    
    Cambios realizados:
    1. Se ha implementado el método get en el archivo rockets.js para realizar una solicitud GET y obtener la lista de todos los cohetes.
    2. Se ha implementado el método getById en el archivo rockets.js para realizar una solicitud GET y obtener los detalles de un cohete específico por su ID.
    3. Ambos métodos devuelven la información de los cohetes en formato JSON.

--- js/module/rockets.js
+++ js/module/rockets.js
@@ -4,3 +4,10 @@ export const getAllRockets = async () => {
     let data = await res.json();
     return data;
 }
+
+export const getRocketById = async (id) => {
+
+    let res = await fetch(`https://api.spacexdata.com/v4/rockets/${id}`);
+    let data = await res.json();
+    return data;
+}
