5162f5c68c2be2160b1b9e776d45ad25ed6e9bfb
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-07-02 09:07:57 -0500

    feat: :sparkles: Implementar paginación de contenido Rocket
    Se ha implementado la funcionalidad de paginación del contenido Rocket en el archivo index.html utilizando JavaScript asíncrono.

---
+++ js/components/pagination.js
@@ -0,0 +1,37 @@
+// import { getAllRockets, getRocketById } from "../../js/module/rockets.js";
+import { getAllRockets, getRocketById } from "./../module/rockets.js";
+
+const getRocketsId = async(e)=>{
+    e.preventDefault();
+    // console.log(e.target);
+    let a = e.target.parentElement.children;
+    for(let val of a){
+        val.classList.remove('activo');
+    }
+    e.target.classList.add('activo');
+
+    let Rocket = await getAllRocketsId(e.target.id);
+    await clear();
+
+
+    await nameRockets(Rocket.name);
+}
+
+export const paginationRockets = async()=>{
+    let rockets = await getAllRockets();
+    let div = document.createElement("div");
+    div.classList.add("buttom__paginacion")
+  
+    rockets.forEach((val,id) => {
+        let a = document.createElement("a");
+        a.setAttribute("href","#");
+        a.id = val.id;
+        a.textContent = id+1;
+        a.addEventListener("click", getRocketsId)
+        div.appendChild(a);
+    });
+    
+    let [a1,a2,a3,a4] = div.children
+    a3.click();
+    return div;
+}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
 import { getAllRockets, getRocketById } from "./module/rockets.js";
+import { 
+    paginationRockets
+} from "./components/pagination.js";
 
 //console.table(await getAllRockets());
-console.table(await getRocketById("5e9d0d95eda69955f709d1eb"));
-
-document.addEventListener("DOMContentLoaded",  async()=>{
-    const params = new URLSearchParams(location.search);
+//console.table(await getRocketById("5e9d0d95eda69955f709d1eb"));
+const headerRockets = document.querySelector(".header");
+const mainOverview = document.querySelector(".main__overview");
+const mainDescriptionLeft = document.querySelector(".main__description__left");
+// document.addEventListener("DOMContentLoaded",  async()=>{
+//     const params = new URLSearchParams(location.search);
     
-});
+//     let receivedAllRocket = await getAllRockets();
+//     let loadImageInnerMainOverview = '';
+//     console.log(receivedAllRocket);
+//     headerRockets.innerHTML = `<h1>${receivedAllRocket[0].name}</h1>`;
+//     //mainDescriptionLeft.innerHTML = 
+//     // receivedAllRocket[0].flickr_images.forEach(individualImage => {
+//     //     loadImageInnerMainOverview += `<img src="${individualImage}" alt="" referrerpolicy="no-referrer">`
+//     // });
+//     //mainOverview.innerHTML = loadImageInnerMainOverview;
+// });
+
+let footerSelect = async(e, id)=>{
+    e.preventDefault();
+    await load();
+    let li = document.querySelectorAll(".footer ul li")
+    for(let val of li){
+        let [a] = val.children
+        a.classList.remove('select');
+    }
+    let [a] = id.children
+    a.classList.add('select');   
+}
+
+let rocket = document.querySelector("#rocket")
+rocket.addEventListener("click", async(e)=>{
+    await footerSelect(e, rocket)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationRockets())
+} )
