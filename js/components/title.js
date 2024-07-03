7f39c8e6288d0ed6a92f8824ad28d55153a900fd
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-07-03 06:57:09 -0500

    feat: :sparkles: Modificar nombre de función para mostrar información de cápsulas
    Se ha modificado el nombre de la función para mostrar la información de cápsulas a uno más genérico que pueda ser utilizado para cualquier tipo de información.

--- js/components/title.js
+++ js/components/title.js
@@ -1,4 +1,4 @@
-export const nameRockets = async(name)=>{
+export const mainTitle = async(name)=>{
     let header__title = document.querySelector("#header__title");
     header__title.innerHTML = "";
     header__title.textContent = name;
